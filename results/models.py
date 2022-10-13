90a82cdf3903cd8bf22fa73f1362630074bd559b
kinganuoluwa <adekogbonking03@gmail.com>
2022-10-13 18:26:08 +0100

    changed pollingunit name limit to 100

--- results/models.py
+++ results/models.py
@@ -16,7 +16,7 @@ class PollingUnit(models.Model):
     agent_first_name = models.CharField(max_length=30, blank=True, null=True)
     agent_last_name = models.CharField(max_length=30, blank=True, null=True)
     agent_phone_number = models.PositiveBigIntegerField(blank=True, null=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return f'{self.name}'
